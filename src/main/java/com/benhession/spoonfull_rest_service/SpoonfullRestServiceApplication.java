9fc5e7af4771e142867abff1c9c82ff34db134d1
Ben Hession <bshession@gmail.com>
2020-07-25 14:52:36 +0100

    Initial commit

---
+++ src/main/java/com/benhession/spoonfull_rest_service/SpoonfullRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.benhession.spoonfull_rest_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoonfullRestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpoonfullRestServiceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/benhession/spoonfull_rest_service/model/Recipe.java
@@ -0,0 +1,64 @@
+package com.benhession.spoonfull_rest_service.model;
+
+import lombok.Data;
+import org.hibernate.annotations.RowId;
+
+import javax.persistence.*;
+import java.time.Duration;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "recipe")
+public class Recipe {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "recipe_id")
+    private int recipeID;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "prep_time")
+    private Duration prepTime;
+
+    @Column(name = "cook_time")
+    private Duration cookTime;
+
+    @Column(name = "total_time")
+    private Duration totalTime;
+
+    @Column(name = "yield")
+    private String yield;
+
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @Column(name = "source_url")
+    private String sourceUrl;
+
+    @Column(name = "hash_value")
+    private String hashValue;
+
+    @ElementCollection
+    @CollectionTable(name = "given_categories")
+    @Column(length = 500, name = "category")
+    private List<String> givenCategories;
+
+    @ElementCollection
+    @CollectionTable(name = "ingredients")
+    @Column(length = 1000, name = "ingredient")
+    private List<String> ingredients;
+
+    @ElementCollection
+    @CollectionTable(name = "method")
+    @Column(length = 5000, name = "step")
+    private List<String> method;
+
+    @ElementCollection
+    @CollectionTable(name = "keywords")
+    @Column(name = "keyword")
+    private List<String> keywords;
+
+}
