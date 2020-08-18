bbb4b71dc3be624f65a0ff97e4796632b9ff088a
Ben Hession <bshession@gmail.com>
2020-08-18 22:52:06 +0100

    Fix HATEOAS implementation and remove unnecessary endpoints.

--- src/main/java/com/benhession/spoonfull_rest_service/controllers/RecipeController.java
+++ src/main/java/com/benhession/spoonfull_rest_service/controllers/RecipeController.java
@@ -27,63 +27,13 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
-    @GetMapping
-    public ResponseEntity<CollectionModel<RecipeEntity>> findRecipes(@RequestParam Optional<Integer> page,
-                                                            @RequestParam Optional<Integer> size) {
-
-        int numOfRecipes = recipeService.count();
-        Iterable<Recipe> recipes;
-        ResponseEntity<CollectionModel<RecipeEntity>> response;
-
-        Optional<PageRequest> pageRequest = (page.isPresent() && size.isPresent())
-                ? Optional.of(PageRequest.of(page.get(), size.get()))
-                : Optional.empty();
-
-        recipes = pageRequest.isPresent()
-                ? recipeService.recipesFrom(pageRequest.get())
-                : recipeService.findAll();
-
-        CollectionModel<RecipeEntity> entities = new RecipeEntityAssembler().toCollectionModel(recipes);
-
-        response = pageRequest.map(
-                request ->
-                        pageableRecipeResponse(entities, request, numOfRecipes)
-        )
-                .orElseGet(() -> {
-
-                    entities.add(
-                            WebMvcLinkBuilder.linkTo(
-                                    WebMvcLinkBuilder.methodOn(RecipeController.class).findRecipes(page, size)
-                            ).withRel("self")
-                    );
-
-                    return new ResponseEntity<>(entities, HttpStatus.OK);
-                });
-
-        return response;
-    }
-
     @GetMapping("/{id}")
     public ResponseEntity<RecipeEntity> recipeById(@PathVariable("id") int id) {
         Optional<Recipe> recipe = recipeService.recipeFromId(id);
 
         return recipe.map(
                 theRecipe -> new ResponseEntity<>(new RecipeEntityAssembler().toModel(theRecipe), HttpStatus.OK)
-        ).orElseGet(
-                () -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND)
-        );
-    }
-
-    @GetMapping("find-by-ingredients")
-    public ResponseEntity<CollectionModel<RecipeEntity>> recipeByIngredients(@RequestParam List<String> ingredients,
-                                                            @RequestParam int page, @RequestParam int size) {
-
-        List<Integer> recipeIds = recipeService.findRecipeIdsFromIngredientIn(ingredients);
-
-        return produceResponseFromIds(recipeIds, page, size);
-
-
+        ).orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
     }
 
     @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
@@ -94,7 +44,10 @@ public class RecipeController {
                                                                    @RequestParam Optional<List<String>> categories,
                                                                    @RequestParam int page, @RequestParam int size) {
 
+        boolean pageInRange;
+        Page<Recipe> recipes;
         Set<Integer> ids = new HashSet<>();
+        CollectionModel<RecipeEntity> recipeEntities;
 
         name.ifPresent(theName -> ids.addAll(recipeService.findRecipeIdFromName(theName)));
 
@@ -104,112 +57,87 @@ public class RecipeController {
 
         categories.ifPresent(c -> addToOrRetainIds(ids, c, recipeService::findRecipesFromGivenCategoriesIn));
 
-        return produceResponseFromIds(ids, page, size);
-
-    }
-
-    private interface CanFindIds {
-        Collection<Integer> findIds(List<String> searchWords);
-    }
-
-    private void addToOrRetainIds(Set<Integer> ids, List<String> param, CanFindIds lambda) {
-
-        if(ids.isEmpty()) {
-            ids.addAll(lambda.findIds(param));
-        } else {
-            ids.retainAll(lambda.findIds(param));
-        }
-    }
-
-    private ResponseEntity<CollectionModel<RecipeEntity>> produceResponseFromIds(Collection<Integer> ids, int page, int size) {
-
-        Optional<ResponseEntity<CollectionModel<RecipeEntity>>> response = Optional.empty();
-
-        if(!ids.isEmpty()) {
+        if (!ids.isEmpty()) {
             PageRequest pageRequest = PageRequest.of(page, size);
-            Page<Recipe> recipes = recipeService.findRecipesFromIdIn(ids, pageRequest);
+            recipes = recipeService.findRecipesFromIdIn(ids, pageRequest);
+            pageInRange = page < recipes.getTotalPages();
 
-            CollectionModel<RecipeEntity> entities = new RecipeEntityAssembler().toCollectionModel(recipes);
-            response = Optional.of(pageableRecipeResponse(entities, pageRequest, ids.size()));
+            recipeEntities = new RecipeEntityAssembler().toCollectionModel(recipes);
+
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
         }
 
-        return response.orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NO_CONTENT));
-    }
-
-    private ResponseEntity<CollectionModel<RecipeEntity>> pageableRecipeResponse(CollectionModel<RecipeEntity> entities,
-                                                                PageRequest request, int totalNumOfEntities) {
-
-        boolean pageInRange = true;
-
-        int firstPage = request.first().getPageNumber();
-        int lastPage =  totalNumOfEntities / request.getPageSize();
-        int currentPage = request.getPageNumber();
-        int pageSize = request.getPageSize();
-
-        if (totalNumOfEntities % pageSize == 0) {
-            lastPage -= 1;
-        }
-
-        if (currentPage > lastPage) {
-            pageInRange = false;
-        }
-
-        entities.add(
+        recipeEntities.add(
                 WebMvcLinkBuilder.linkTo(
-                        WebMvcLinkBuilder.methodOn(RecipeController.class).findRecipes(
-                                Optional.of(currentPage),
-                                Optional.of(pageSize)
-                        )
-                ).withRel("self")
+                        WebMvcLinkBuilder.methodOn(RecipeController.class)
+                                .recipesBy(name, ingredients, keywords, categories, page, size)
+                ).withRel("self").expand()
         );
 
-        entities.add(
+        recipeEntities.add(
                 WebMvcLinkBuilder.linkTo(
-                        WebMvcLinkBuilder.methodOn(RecipeController.class).findRecipes(
-                                Optional.of(firstPage),
-                                Optional.of(pageSize)
-                        )
-                ).withRel("firstPage")
-
+                        WebMvcLinkBuilder.methodOn(RecipeController.class)
+                                .recipesBy(name, ingredients, keywords, categories, 0, size)
+                ).withRel("first_page").expand()
         );
 
-        if (currentPage != firstPage) {
-            entities.add(
+        if (!recipes.isFirst()) {
+            recipeEntities.add(
                     WebMvcLinkBuilder.linkTo(
-                            WebMvcLinkBuilder.methodOn(RecipeController.class).findRecipes(
-                                    Optional.of(request.previousOrFirst().getPageNumber()),
-                                    Optional.of(pageSize)
-                            )
-                    ).withRel("previousPage")
+                            WebMvcLinkBuilder.methodOn(RecipeController.class)
+                                    .recipesBy(name, ingredients, keywords, categories,
+                                            recipes.previousOrFirstPageable().getPageNumber(), size)
+                    ).withRel("previous_page").expand()
             );
         }
 
-        if (currentPage != lastPage) {
-            entities.add(
+        if (!recipes.isLast()) {
+            recipeEntities.add(
                     WebMvcLinkBuilder.linkTo(
-                            WebMvcLinkBuilder.methodOn(RecipeController.class).findRecipes(
-                                    Optional.of(request.next().getPageNumber()),
-                                    Optional.of(pageSize)
-                            )
-                    ).withRel("nextPage")
+                            WebMvcLinkBuilder.methodOn(RecipeController.class)
+                                    .recipesBy(name, ingredients, keywords, categories,
+                                            recipes.nextOrLastPageable().getPageNumber(), size)
+                    ).withRel("next_page").expand()
             );
         }
 
-        entities.add(
+        recipeEntities.add(
                 WebMvcLinkBuilder.linkTo(
-                        WebMvcLinkBuilder.methodOn(RecipeController.class).findRecipes(
-                                Optional.of(lastPage),
-                                Optional.of(pageSize)
-                        )
-                ).withRel("lastPage")
+                        WebMvcLinkBuilder.methodOn(RecipeController.class)
+                                .recipesBy(name, ingredients, keywords, categories,
+                                        recipes.getTotalPages() - 1, size)
+                ).withRel("last_page").expand()
         );
 
 
         return pageInRange
-                ? new ResponseEntity<>(entities, HttpStatus.OK)
+                ? new ResponseEntity<>(recipeEntities, HttpStatus.OK)
                 : new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-
 
     }
 
+    /**
+     * Single method interface for a find ids query
+     */
+    private interface IdQuery {
+        Collection<Integer> findIds(List<String> searchWords);
+    }
+
+    /**
+     * Add all ids from a query to the set if it is empty, otherwise retain the items that are present
+     * in the set already
+     *
+     * @param ids       the id set
+     * @param paramList the the list of values from a request parameter
+     * @param idQuery   the method reference or lambda expression that finds the ids
+     */
+    private void addToOrRetainIds(Set<Integer> ids, List<String> paramList, IdQuery idQuery) {
+
+        if (ids.isEmpty()) {
+            ids.addAll(idQuery.findIds(paramList));
+        } else {
+            ids.retainAll(idQuery.findIds(paramList));
+        }
+    }
 }
